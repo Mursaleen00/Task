ffcbd3218dfe26878e3ef9d988aff2d17d32a65e
Mursaleen00 <playessport@gmail.com>
2023-05-14 00:30:17 +0500

    Assignment 1, 2, 3, Completed

---
+++ script.js
@@ -0,0 +1,13 @@
+// Task_1
+
+function showAlert(){
+    alert("Hello Worl!d");
+};
+
+
+
+// Task_3
+function alertText() {
+    var name = document.getElementById("text").value
+    alert(name);
+}
